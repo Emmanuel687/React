b3ead2f94a22ce85b3690e2cc7fe1f6d7bebdc3b
Emmanuel Koech <koechemmanuel2002@gmail.com>
2022-09-14 18:35:23 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+React
+
+Creating React Component
+
+-Create a file with .js extension
+Import React from react
+  import React from ‘react’
+
+-Create a function.
+
+  default function Component(){
+return()
+}
+
+-Return to Root folder and import react component.
+
+Import Component from ‘./Component’’
