164b6c62be2e2695602911a891154f3df67a56b7
Emmanuel Koech <koechemmanuel2002@gmail.com>
2022-09-15 15:14:45 +0300

    Initial commit

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ return()
 -Return to Root folder and import react component.
 
 Import Component from ‘./Component’’
+
+React Properties
+
+-Include Prop in function.
+import React from "react";
+
+export default function Student(props) {
+  return (
+    <h1>
+      {props.firstName} {props.lastName} is {props.age}.
+    </h1>
+  )
+}
+
+-Inside the react component function use props.`name`
+  <h1>
+      {props.firstName} {props.lastName} is {props.age}.
+  </h1>
+-In react root component add variable of what is in your component
+<Student firstName="Julie" lastName="Johnson" age={23} pro={true} />
+
+
+
